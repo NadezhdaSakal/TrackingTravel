c469743c6253ddd452cbb438a0329152cadaa461
NadezhdaSakal <sakaln88@gmail.com>
2023-01-25 00:34:57 +0300

    splash anim add

--- app/src/main/java/com/maximvs/trackingtravel/App.kt
+++ app/src/main/java/com/maximvs/trackingtravel/App.kt
@@ -1,12 +1,6 @@
 package com.maximvs.trackingtravel
 
 import android.app.Application
-import android.content.Intent
-import android.content.SharedPreferences
-import android.preference.PreferenceManager
-//import androidx.preference.PreferenceManager
-import com.maximvs.trackingtravel.view.MainActivity
-import com.maximvs.trackingtravel.view.fragments.StartActivity
 import dagger.hilt.android.HiltAndroidApp
 
 
@@ -17,20 +11,7 @@ class App : Application() {
         super.onCreate()
         instance = this
 
-        val userInfo: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(
-            applicationContext
-        )
-        val editor = userInfo.edit()
-        val logedIn = userInfo.getBoolean("loggedIn", false)
-        if (logedIn) {
-            val intent = Intent(this, MainActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            startActivity(intent)
-        } else {
-            val intent = Intent(this, StartActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            startActivity(intent)
-        }
+
     }
 
     companion object {
@@ -38,6 +19,3 @@ class App : Application() {
             private set
     }
 }
-
-
-
