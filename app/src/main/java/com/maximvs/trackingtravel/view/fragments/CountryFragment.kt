92bb5fdfbcf71512eb14dc64128d9c04726f9c92
NadezhdaSakal <sakaln88@gmail.com>
2023-02-20 23:02:47 +0300

    add radioButtons

--- app/src/main/java/com/maximvs/trackingtravel/view/fragments/CountryFragment.kt
+++ app/src/main/java/com/maximvs/trackingtravel/view/fragments/CountryFragment.kt
@@ -1,13 +1,11 @@
 package com.maximvs.trackingtravel.view.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.maximvs.trackingtravel.R
+import androidx.fragment.app.Fragment
 import com.maximvs.trackingtravel.databinding.FragmentCountryBinding
-import com.maximvs.trackingtravel.databinding.FragmentSettingsBinding
 
 
 class CountryFragment : Fragment() {
