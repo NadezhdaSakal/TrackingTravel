d3f336eda3a19dce5922f218f3f8c0d725ab3cae
NadezhdaSakal <sakaln88@gmail.com>
2023-03-02 09:27:17 +0300

    fix rep

--- app/src/main/java/com/maximvs/trackingtravel/data/MainRepository.kt
+++ app/src/main/java/com/maximvs/trackingtravel/data/MainRepository.kt
@@ -2,9 +2,7 @@ package com.maximvs.trackingtravel.data
 
 import com.maximvs.trackingtravel.data.dao.RouteDao
 import com.maximvs.trackingtravel.data.entity.Route
-import java.util.concurrent.Executors
 import kotlinx.coroutines.flow.Flow
-
 
 
 class MainRepository
@@ -12,10 +10,8 @@ constructor(
     private val routeDao: RouteDao
 ) {
     fun putToDb(routes: List<Route>) {
-        //Запросы в бд должны быть в отдельном потоке
-        Executors.newSingleThreadExecutor().execute {
-            routeDao.insertAll(routes)
-        }
+        routeDao.insertAll(routes)
+
     }
 
     fun getAllFromDB(): Flow<List<Route>> {
